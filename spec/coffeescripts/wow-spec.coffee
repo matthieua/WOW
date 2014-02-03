8f5538027711b869d7996de806481a91add92347
Attila Oláh <attilaolah@gmail.com>
2014-02-03 14:57:08 +0700

    add some tests

--- spec/coffeescripts/wow-spec.coffee
+++ spec/coffeescripts/wow-spec.coffee
@@ -1,8 +1,27 @@
-describe 'WOW', ->
+describe "WOW", ->
   beforeEach ->
-    loadFixtures 'fragment.html'
-    @$element = $('#fixtures')
+    loadFixtures "fragment.html"
+    @$element = $ "#fixtures"
 
-  describe 'library behavior', ->
-    it 'exists', ->
-      expect( WOW ).toBeDefined()
+  describe "test environment", ->
+
+    it "should emulate a window of height 300", ->
+      expect document.documentElement.clientHeight
+        .toBe 300
+
+  describe "library behavior", ->
+
+    it "exists", ->
+      expect WOW
+        .toBeDefined()
+
+    it "has an 'init' method", ->
+      expect new WOW().init
+        .toBeDefined()
+
+    xit "animates elements already visible on the page", ->
+      new WOW().init()
+      #expect $("#test-1")[0]
+      #  .toHaveClass "animated"
+      expect $("#test-2")[0]
+        .toHaveClass "animated"
--- spec/javascripts/wow-spec.js
+++ spec/javascripts/wow-spec.js
@@ -1,12 +1,24 @@
 (function() {
-  describe('WOW', function() {
+  describe("WOW", function() {
     beforeEach(function() {
-      loadFixtures('fragment.html');
-      return this.$element = $('#fixtures');
+      loadFixtures("fragment.html");
+      return this.$element = $("#fixtures");
     });
-    return describe('library behavior', function() {
-      return it('exists', function() {
+    describe("test environment", function() {
+      return it("should emulate a window of height 300", function() {
+        return expect(document.documentElement.clientHeight).toBe(300);
+      });
+    });
+    return describe("library behavior", function() {
+      it("exists", function() {
         return expect(WOW).toBeDefined();
+      });
+      it("has an 'init' method", function() {
+        return expect(new WOW().init).toBeDefined();
+      });
+      return xit("animates elements already visible on the page", function() {
+        new WOW().init();
+        return expect($("#test-2")[0]).toHaveClass("animated");
       });
     });
   });
