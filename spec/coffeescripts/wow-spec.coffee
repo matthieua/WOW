80a45de5c3de4703d4fe7f93dcf75cf5d10a4694
Attila Oláh <attilaolah@gmail.com>
2014-05-07 18:52:41 +0700

    use-single-quotes

--- spec/coffeescripts/wow-spec.coffee
+++ spec/coffeescripts/wow-spec.coffee
@@ -1,4 +1,4 @@
-describe "WOW", ->
+describe 'WOW', ->
 
   # Time to wait after each scroll event:
   # (This should be >= the interval used by the plugin.)
@@ -7,53 +7,53 @@ describe "WOW", ->
   # Height of the PhantomJS window:
   winHeight = 300
 
-  describe "simple test environment", ->
+  describe 'simple test environment', ->
 
     beforeEach ->
-      loadFixtures "simple.html"
+      loadFixtures 'simple.html'
 
-    it "emulates window height", ->
+    it 'emulates window height', ->
       expect document.documentElement.clientHeight
         .toBe winHeight
 
-    it "has boxes set up for testing", ->
+    it 'has boxes set up for testing', ->
       boxHeight = 200
       boxCount = $ '#simple'
         .children().length
       # Check each box height
-      expect $("#simple").height()
+      expect $('#simple').height()
         .toBe boxHeight*boxCount
-      expect $("#simple-1").height()
+      expect $('#simple-1').height()
         .toBe boxHeight
-      expect $("#simple-2").height()
+      expect $('#simple-2').height()
         .toBe boxHeight
-      expect $("#simple-3").height()
+      expect $('#simple-3').height()
         .toBe boxHeight
-      expect $("#simple-4").height()
+      expect $('#simple-4').height()
         .toBe boxHeight
-      expect $("#simple-5").height()
+      expect $('#simple-5').height()
         .toBe boxHeight
       # Check each box offset
-      offset = $("#simple").offset().top
-      expect $("#simple-1").offset().top
+      offset = $('#simple').offset().top
+      expect $('#simple-1').offset().top
         .toBe offset + boxHeight*0
-      expect $("#simple-2").offset().top
+      expect $('#simple-2').offset().top
         .toBe offset + boxHeight*1
-      expect $("#simple-3").offset().top
+      expect $('#simple-3').offset().top
         .toBe offset + boxHeight*2
-      expect $("#simple-4").offset().top
+      expect $('#simple-4').offset().top
         .toBe offset + boxHeight*3
-      expect $("#simple-5").offset().top
+      expect $('#simple-5').offset().top
         .toBe offset + boxHeight*4
-      style = $("#simple-5")[0].style
+      style = $('#simple-5')[0].style
       expect style.background
         .toBe 'yellow'
       expect style.color
         .toBe 'red'
 
-  describe "library smoke test", ->
+  describe 'library smoke test', ->
 
-    it "exists", ->
+    it 'exists', ->
       expect WOW
         .toBeDefined()
 
@@ -61,69 +61,69 @@ describe "WOW", ->
       expect new WOW().init
         .toBeDefined()
 
-  describe "library behaviour", ->
+  describe 'library behaviour', ->
 
     beforeEach (done) ->
-      loadFixtures "simple.html"
+      loadFixtures 'simple.html'
       new WOW().init()
       setTimeout ->
         done()
       , timeout
 
-    it "animates elements that are fully visible on the page", ->
-      expect $ "#simple-1"
-        .toHaveClass "animated"
-      expect $("#simple-1").css "visibility"
-        .toBe "visible"
+    it 'animates elements that are fully visible on the page', ->
+      expect $ '#simple-1'
+        .toHaveClass 'animated'
+      expect $('#simple-1').css 'visibility'
+        .toBe 'visible'
 
     it "does not touch elements that don't have the marker class", ->
-      expect $ "#simple-2"
-        .not.toHaveClass "animated"
-      expect $("#simple-2").css "visibility"
-        .toBe "visible"
+      expect $ '#simple-2'
+        .not.toHaveClass 'animated'
+      expect $('#simple-2').css 'visibility'
+        .toBe 'visible'
 
-    it "does not animate elements not yet visible on the page", ->
-      expect $ "#simple-3"
-        .not.toHaveClass "animated"
-      expect $("#simple-3").css "visibility"
-        .not.toBe "visible"
-      expect $ "#simple-4"
-        .not.toHaveClass "animated"
-      expect $("#simple-4").css "visibility"
-        .not.toBe "visible"
+    it 'does not animate elements not yet visible on the page', ->
+      expect $ '#simple-3'
+        .not.toHaveClass 'animated'
+      expect $('#simple-3').css 'visibility'
+        .not.toBe 'visible'
+      expect $ '#simple-4'
+        .not.toHaveClass 'animated'
+      expect $('#simple-4').css 'visibility'
+        .not.toBe 'visible'
 
-    it "animates elements after scrolling down and they become visible", (done) ->
+    it 'animates elements after scrolling down and they become visible', (done) ->
       # Scroll down so that 150px of #simple-3 becomes visible.
-      window.scrollTo 0, $("#simple-3").offset().top - winHeight + 150
+      window.scrollTo 0, $('#simple-3').offset().top - winHeight + 150
       setTimeout ->
-        expect $ "#simple-3"
-          .toHaveClass "animated"
-        expect $("#simple-3").css "visibility"
-          .toBe "visible"
-        expect $ "#simple-4"
-          .not.toHaveClass "animated"
-        expect $("#simple-4").css "visibility"
-          .not.toBe "visible"
+        expect $ '#simple-3'
+          .toHaveClass 'animated'
+        expect $('#simple-3').css 'visibility'
+          .toBe 'visible'
+        expect $ '#simple-4'
+          .not.toHaveClass 'animated'
+        expect $('#simple-4').css 'visibility'
+          .not.toBe 'visible'
         # Scroll down so that 150px of #simple-4 becomes visible.
-        window.scrollTo 0, $("#simple-4").offset().top - winHeight + 150
+        window.scrollTo 0, $('#simple-4').offset().top - winHeight + 150
         setTimeout ->
-          expect $ "#simple-4"
-            .toHaveClass "animated"
-          expect $("#simple-4").css "visibility"
-            .toBe "visible"
+          expect $ '#simple-4'
+            .toHaveClass 'animated'
+          expect $('#simple-4').css 'visibility'
+            .toBe 'visible'
           done()
         , timeout
       , timeout
 
-    it "does not tamper with the style attribute", (done) ->
+    it 'does not tamper with the style attribute', (done) ->
       # Scroll down so that 150px of #simple-4 becomes visible.
-      window.scrollTo 0, $("#simple-5").offset().top - winHeight + 150
+      window.scrollTo 0, $('#simple-5').offset().top - winHeight + 150
       setTimeout ->
-        expect $ "#simple-5"
-          .toHaveClass "animated"
-        expect $("#simple-5").css "visibility"
-          .toBe "visible"
-        style = $("#simple-5")[0].style
+        expect $ '#simple-5'
+          .toHaveClass 'animated'
+        expect $('#simple-5').css 'visibility'
+          .toBe 'visible'
+        style = $('#simple-5')[0].style
         expect style.background
           .toBe 'yellow'
         expect style.color
@@ -131,45 +131,45 @@ describe "WOW", ->
         done()
       , timeout
 
-  describe "custom test environment", ->
+  describe 'custom test environment', ->
 
     beforeEach ->
-      loadFixtures "custom.html"
+      loadFixtures 'custom.html'
 
-    it "emulates window height", ->
+    it 'emulates window height', ->
       expect document.documentElement.clientHeight
         .toBe winHeight
 
-    it "has boxes set up for testing", ->
+    it 'has boxes set up for testing', ->
       # Check each box height
-      expect $("#custom").height()
+      expect $('#custom').height()
         .toBe 800
-      expect $("#custom-1").height()
+      expect $('#custom-1').height()
         .toBe 200
-      expect $("#custom-2").height()
+      expect $('#custom-2').height()
         .toBe 200
-      expect $("#custom-3").height()
+      expect $('#custom-3').height()
         .toBe 200
-      expect $("#custom-4").height()
+      expect $('#custom-4').height()
         .toBe 200
       # Check each box offset
-      offset = $("#custom").offset().top
-      expect $("#custom-1").offset().top
+      offset = $('#custom').offset().top
+      expect $('#custom-1').offset().top
         .toBe offset + 200*0
-      expect $("#custom-2").offset().top
+      expect $('#custom-2').offset().top
         .toBe offset + 200*1
-      expect $("#custom-3").offset().top
+      expect $('#custom-3').offset().top
         .toBe offset + 200*2
-      expect $("#custom-4").offset().top
+      expect $('#custom-4').offset().top
         .toBe offset + 200*3
 
-  describe "library behaviour with custom settings", ->
+  describe 'library behaviour with custom settings', ->
 
     beforeEach (done) ->
-      loadFixtures "custom.html"
+      loadFixtures 'custom.html'
       new WOW
-        boxClass:     "block"
-        animateClass: "fancy"
+        boxClass:     'block'
+        animateClass: 'fancy'
         offset:       10
       .init()
       setTimeout ->
@@ -178,61 +178,61 @@ describe "WOW", ->
 
     it "does not touch elements that don't have the marker class", (done) ->
       # Scroll down so that 15px of #custom-1 becomes visible.
-      window.scrollTo 0, $("#custom-1").offset().top - winHeight + 15
+      window.scrollTo 0, $('#custom-1').offset().top - winHeight + 15
       setTimeout ->
-        expect $ "#custom-1"
-          .not.toHaveClass "fancy"
+        expect $ '#custom-1'
+          .not.toHaveClass 'fancy'
         done()
       , timeout
 
     it "animates elements that are partially visible on the page based on the 'offset' config", (done) ->
       setTimeout ->
         # Scroll down so that 5px of #custom-2 becomes visible.
-        window.scrollTo 0, $("#custom-2").offset().top - winHeight + 5
-        expect $ "#custom-2"
-          .not.toHaveClass "fancy"
-        window.scrollTo 0, $("#custom-2").offset().top - winHeight + 15
+        window.scrollTo 0, $('#custom-2').offset().top - winHeight + 5
+        expect $ '#custom-2'
+          .not.toHaveClass 'fancy'
+        window.scrollTo 0, $('#custom-2').offset().top - winHeight + 15
         setTimeout ->
           # Scroll down so that 15px of #custom-2 becomes visible.
-          expect $ "#custom-2"
-            .toHaveClass "fancy"
-          expect $("#custom-2").css "visibility"
-            .toBe "visible"
+          expect $ '#custom-2'
+            .toHaveClass 'fancy'
+          expect $('#custom-2').css 'visibility'
+            .toBe 'visible'
           done()
         , timeout
       , timeout
 
-    it "does not animate elements not yet visible on the page", ->
-      expect $ "#custom-3"
-        .not.toHaveClass "fancy"
-      expect $ "#custom-4"
-        .not.toHaveClass "fancy"
+    it 'does not animate elements not yet visible on the page', ->
+      expect $ '#custom-3'
+        .not.toHaveClass 'fancy'
+      expect $ '#custom-4'
+        .not.toHaveClass 'fancy'
 
-    it "animates elements after scrolling down and they become visible", (done) ->
+    it 'animates elements after scrolling down and they become visible', (done) ->
       # Scroll down so that 150px of #custom-3 becomes visible.
-      window.scrollTo 0, $("#custom-3").offset().top - winHeight + 150
+      window.scrollTo 0, $('#custom-3').offset().top - winHeight + 150
       setTimeout ->
-        expect $ "#custom-3"
-          .toHaveClass "fancy"
-        expect $("#custom-3").css "visibility"
-          .toBe "visible"
-        expect $("#custom-3")[0].style.webkitAnimationIterationCount
-          .toBe "2"
-        expect $ "#custom-4"
-          .not.toHaveClass "fancy"
+        expect $ '#custom-3'
+          .toHaveClass 'fancy'
+        expect $('#custom-3').css 'visibility'
+          .toBe 'visible'
+        expect $('#custom-3')[0].style.webkitAnimationIterationCount
+          .toBe '2'
+        expect $ '#custom-4'
+          .not.toHaveClass 'fancy'
         # Scroll down so that 150px of #custom-4 becomes visible.
-        window.scrollTo 0, $("#custom-4").offset().top - winHeight + 150
+        window.scrollTo 0, $('#custom-4').offset().top - winHeight + 150
         setTimeout ->
-          expect $ "#custom-4"
-            .toHaveClass "fancy"
-          expect $("#custom-4").css "visibility"
-            .toBe "visible"
-          expect $("#custom-4")[0].style.webkitAnimationIterationCount
-            .toBe "infinite"
-          expect $("#custom-4")[0].style.webkitAnimationDuration
-            .toBe "2s"
-          expect $("#custom-4")[0].style.webkitAnimationDelay
-            .toBe "1s"
+          expect $ '#custom-4'
+            .toHaveClass 'fancy'
+          expect $('#custom-4').css 'visibility'
+            .toBe 'visible'
+          expect $('#custom-4')[0].style.webkitAnimationIterationCount
+            .toBe 'infinite'
+          expect $('#custom-4')[0].style.webkitAnimationDuration
+            .toBe '2s'
+          expect $('#custom-4')[0].style.webkitAnimationDelay
+            .toBe '1s'
           done()
         , timeout
       , timeout
--- spec/javascripts/wow-spec.js
+++ spec/javascripts/wow-spec.js
@@ -1,121 +1,121 @@
 (function() {
-  describe("WOW", function() {
+  describe('WOW', function() {
     var timeout, winHeight;
     timeout = 100;
     winHeight = 300;
-    describe("simple test environment", function() {
+    describe('simple test environment', function() {
       beforeEach(function() {
-        return loadFixtures("simple.html");
+        return loadFixtures('simple.html');
       });
-      it("emulates window height", function() {
+      it('emulates window height', function() {
         return expect(document.documentElement.clientHeight).toBe(winHeight);
       });
-      return it("has boxes set up for testing", function() {
+      return it('has boxes set up for testing', function() {
         var boxCount, boxHeight, offset, style;
         boxHeight = 200;
         boxCount = $('#simple').children().length;
-        expect($("#simple").height()).toBe(boxHeight * boxCount);
-        expect($("#simple-1").height()).toBe(boxHeight);
-        expect($("#simple-2").height()).toBe(boxHeight);
-        expect($("#simple-3").height()).toBe(boxHeight);
-        expect($("#simple-4").height()).toBe(boxHeight);
-        expect($("#simple-5").height()).toBe(boxHeight);
-        offset = $("#simple").offset().top;
-        expect($("#simple-1").offset().top).toBe(offset + boxHeight * 0);
-        expect($("#simple-2").offset().top).toBe(offset + boxHeight * 1);
-        expect($("#simple-3").offset().top).toBe(offset + boxHeight * 2);
-        expect($("#simple-4").offset().top).toBe(offset + boxHeight * 3);
-        expect($("#simple-5").offset().top).toBe(offset + boxHeight * 4);
-        style = $("#simple-5")[0].style;
+        expect($('#simple').height()).toBe(boxHeight * boxCount);
+        expect($('#simple-1').height()).toBe(boxHeight);
+        expect($('#simple-2').height()).toBe(boxHeight);
+        expect($('#simple-3').height()).toBe(boxHeight);
+        expect($('#simple-4').height()).toBe(boxHeight);
+        expect($('#simple-5').height()).toBe(boxHeight);
+        offset = $('#simple').offset().top;
+        expect($('#simple-1').offset().top).toBe(offset + boxHeight * 0);
+        expect($('#simple-2').offset().top).toBe(offset + boxHeight * 1);
+        expect($('#simple-3').offset().top).toBe(offset + boxHeight * 2);
+        expect($('#simple-4').offset().top).toBe(offset + boxHeight * 3);
+        expect($('#simple-5').offset().top).toBe(offset + boxHeight * 4);
+        style = $('#simple-5')[0].style;
         expect(style.background).toBe('yellow');
         return expect(style.color).toBe('red');
       });
     });
-    describe("library smoke test", function() {
-      it("exists", function() {
+    describe('library smoke test', function() {
+      it('exists', function() {
         return expect(WOW).toBeDefined();
       });
       return it("has an 'init' method", function() {
         return expect(new WOW().init).toBeDefined();
       });
     });
-    describe("library behaviour", function() {
+    describe('library behaviour', function() {
       beforeEach(function(done) {
-        loadFixtures("simple.html");
+        loadFixtures('simple.html');
         new WOW().init();
         return setTimeout(function() {
           return done();
         }, timeout);
       });
-      it("animates elements that are fully visible on the page", function() {
-        expect($("#simple-1")).toHaveClass("animated");
-        return expect($("#simple-1").css("visibility")).toBe("visible");
+      it('animates elements that are fully visible on the page', function() {
+        expect($('#simple-1')).toHaveClass('animated');
+        return expect($('#simple-1').css('visibility')).toBe('visible');
       });
       it("does not touch elements that don't have the marker class", function() {
-        expect($("#simple-2")).not.toHaveClass("animated");
-        return expect($("#simple-2").css("visibility")).toBe("visible");
+        expect($('#simple-2')).not.toHaveClass('animated');
+        return expect($('#simple-2').css('visibility')).toBe('visible');
       });
-      it("does not animate elements not yet visible on the page", function() {
-        expect($("#simple-3")).not.toHaveClass("animated");
-        expect($("#simple-3").css("visibility")).not.toBe("visible");
-        expect($("#simple-4")).not.toHaveClass("animated");
-        return expect($("#simple-4").css("visibility")).not.toBe("visible");
+      it('does not animate elements not yet visible on the page', function() {
+        expect($('#simple-3')).not.toHaveClass('animated');
+        expect($('#simple-3').css('visibility')).not.toBe('visible');
+        expect($('#simple-4')).not.toHaveClass('animated');
+        return expect($('#simple-4').css('visibility')).not.toBe('visible');
       });
-      it("animates elements after scrolling down and they become visible", function(done) {
-        window.scrollTo(0, $("#simple-3").offset().top - winHeight + 150);
+      it('animates elements after scrolling down and they become visible', function(done) {
+        window.scrollTo(0, $('#simple-3').offset().top - winHeight + 150);
         return setTimeout(function() {
-          expect($("#simple-3")).toHaveClass("animated");
-          expect($("#simple-3").css("visibility")).toBe("visible");
-          expect($("#simple-4")).not.toHaveClass("animated");
-          expect($("#simple-4").css("visibility")).not.toBe("visible");
-          window.scrollTo(0, $("#simple-4").offset().top - winHeight + 150);
+          expect($('#simple-3')).toHaveClass('animated');
+          expect($('#simple-3').css('visibility')).toBe('visible');
+          expect($('#simple-4')).not.toHaveClass('animated');
+          expect($('#simple-4').css('visibility')).not.toBe('visible');
+          window.scrollTo(0, $('#simple-4').offset().top - winHeight + 150);
           return setTimeout(function() {
-            expect($("#simple-4")).toHaveClass("animated");
-            expect($("#simple-4").css("visibility")).toBe("visible");
+            expect($('#simple-4')).toHaveClass('animated');
+            expect($('#simple-4').css('visibility')).toBe('visible');
             return done();
           }, timeout);
         }, timeout);
       });
-      return it("does not tamper with the style attribute", function(done) {
-        window.scrollTo(0, $("#simple-5").offset().top - winHeight + 150);
+      return it('does not tamper with the style attribute', function(done) {
+        window.scrollTo(0, $('#simple-5').offset().top - winHeight + 150);
         return setTimeout(function() {
           var style;
-          expect($("#simple-5")).toHaveClass("animated");
-          expect($("#simple-5").css("visibility")).toBe("visible");
-          style = $("#simple-5")[0].style;
+          expect($('#simple-5')).toHaveClass('animated');
+          expect($('#simple-5').css('visibility')).toBe('visible');
+          style = $('#simple-5')[0].style;
           expect(style.background).toBe('yellow');
           expect(style.color).toBe('red');
           return done();
         }, timeout);
       });
     });
-    describe("custom test environment", function() {
+    describe('custom test environment', function() {
       beforeEach(function() {
-        return loadFixtures("custom.html");
+        return loadFixtures('custom.html');
       });
-      it("emulates window height", function() {
+      it('emulates window height', function() {
         return expect(document.documentElement.clientHeight).toBe(winHeight);
       });
-      return it("has boxes set up for testing", function() {
+      return it('has boxes set up for testing', function() {
         var offset;
-        expect($("#custom").height()).toBe(800);
-        expect($("#custom-1").height()).toBe(200);
-        expect($("#custom-2").height()).toBe(200);
-        expect($("#custom-3").height()).toBe(200);
-        expect($("#custom-4").height()).toBe(200);
-        offset = $("#custom").offset().top;
-        expect($("#custom-1").offset().top).toBe(offset + 200 * 0);
-        expect($("#custom-2").offset().top).toBe(offset + 200 * 1);
-        expect($("#custom-3").offset().top).toBe(offset + 200 * 2);
-        return expect($("#custom-4").offset().top).toBe(offset + 200 * 3);
+        expect($('#custom').height()).toBe(800);
+        expect($('#custom-1').height()).toBe(200);
+        expect($('#custom-2').height()).toBe(200);
+        expect($('#custom-3').height()).toBe(200);
+        expect($('#custom-4').height()).toBe(200);
+        offset = $('#custom').offset().top;
+        expect($('#custom-1').offset().top).toBe(offset + 200 * 0);
+        expect($('#custom-2').offset().top).toBe(offset + 200 * 1);
+        expect($('#custom-3').offset().top).toBe(offset + 200 * 2);
+        return expect($('#custom-4').offset().top).toBe(offset + 200 * 3);
       });
     });
-    return describe("library behaviour with custom settings", function() {
+    return describe('library behaviour with custom settings', function() {
       beforeEach(function(done) {
-        loadFixtures("custom.html");
+        loadFixtures('custom.html');
         new WOW({
-          boxClass: "block",
-          animateClass: "fancy",
+          boxClass: 'block',
+          animateClass: 'fancy',
           offset: 10
         }).init();
         return setTimeout(function() {
@@ -123,42 +123,42 @@
         }, timeout);
       });
       it("does not touch elements that don't have the marker class", function(done) {
-        window.scrollTo(0, $("#custom-1").offset().top - winHeight + 15);
+        window.scrollTo(0, $('#custom-1').offset().top - winHeight + 15);
         return setTimeout(function() {
-          expect($("#custom-1")).not.toHaveClass("fancy");
+          expect($('#custom-1')).not.toHaveClass('fancy');
           return done();
         }, timeout);
       });
       it("animates elements that are partially visible on the page based on the 'offset' config", function(done) {
         return setTimeout(function() {
-          window.scrollTo(0, $("#custom-2").offset().top - winHeight + 5);
-          expect($("#custom-2")).not.toHaveClass("fancy");
-          window.scrollTo(0, $("#custom-2").offset().top - winHeight + 15);
+          window.scrollTo(0, $('#custom-2').offset().top - winHeight + 5);
+          expect($('#custom-2')).not.toHaveClass('fancy');
+          window.scrollTo(0, $('#custom-2').offset().top - winHeight + 15);
           return setTimeout(function() {
-            expect($("#custom-2")).toHaveClass("fancy");
-            expect($("#custom-2").css("visibility")).toBe("visible");
+            expect($('#custom-2')).toHaveClass('fancy');
+            expect($('#custom-2').css('visibility')).toBe('visible');
             return done();
           }, timeout);
         }, timeout);
       });
-      it("does not animate elements not yet visible on the page", function() {
-        expect($("#custom-3")).not.toHaveClass("fancy");
-        return expect($("#custom-4")).not.toHaveClass("fancy");
+      it('does not animate elements not yet visible on the page', function() {
+        expect($('#custom-3')).not.toHaveClass('fancy');
+        return expect($('#custom-4')).not.toHaveClass('fancy');
       });
-      return it("animates elements after scrolling down and they become visible", function(done) {
-        window.scrollTo(0, $("#custom-3").offset().top - winHeight + 150);
+      return it('animates elements after scrolling down and they become visible', function(done) {
+        window.scrollTo(0, $('#custom-3').offset().top - winHeight + 150);
         return setTimeout(function() {
-          expect($("#custom-3")).toHaveClass("fancy");
-          expect($("#custom-3").css("visibility")).toBe("visible");
-          expect($("#custom-3")[0].style.webkitAnimationIterationCount).toBe("2");
-          expect($("#custom-4")).not.toHaveClass("fancy");
-          window.scrollTo(0, $("#custom-4").offset().top - winHeight + 150);
+          expect($('#custom-3')).toHaveClass('fancy');
+          expect($('#custom-3').css('visibility')).toBe('visible');
+          expect($('#custom-3')[0].style.webkitAnimationIterationCount).toBe('2');
+          expect($('#custom-4')).not.toHaveClass('fancy');
+          window.scrollTo(0, $('#custom-4').offset().top - winHeight + 150);
           return setTimeout(function() {
-            expect($("#custom-4")).toHaveClass("fancy");
-            expect($("#custom-4").css("visibility")).toBe("visible");
-            expect($("#custom-4")[0].style.webkitAnimationIterationCount).toBe("infinite");
-            expect($("#custom-4")[0].style.webkitAnimationDuration).toBe("2s");
-            expect($("#custom-4")[0].style.webkitAnimationDelay).toBe("1s");
+            expect($('#custom-4')).toHaveClass('fancy');
+            expect($('#custom-4').css('visibility')).toBe('visible');
+            expect($('#custom-4')[0].style.webkitAnimationIterationCount).toBe('infinite');
+            expect($('#custom-4')[0].style.webkitAnimationDuration).toBe('2s');
+            expect($('#custom-4')[0].style.webkitAnimationDelay).toBe('1s');
             return done();
           }, timeout);
         }, timeout);
