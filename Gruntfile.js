5ea68b1a4f1d2aafa87e6f3eeba2dcc5e6530894
Matt Aussaguel <matthieu.aussaguel@gmail.com>
2016-04-08 11:40:22 -0700

    Update license details

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
           '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
           '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
           '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-          ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */',
+          '*/',
         report: 'gzip'
       }
     },
